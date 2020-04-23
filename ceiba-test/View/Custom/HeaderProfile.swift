f72774513dc1f16abb3ea3846a9263c398528953
JCkshone <jcinfonet.kshone@gmail.com>
2020-04-22 23:25:21 -0500

    issues corrections

---
+++ ceiba-test/View/Custom/HeaderProfile.swift
@@ -0,0 +1,13 @@
+//
+//  HeaderProfile.swift
+//  ceiba-test
+//
+//  Created by Jc on 22/04/20.
+//  Copyright © 2020 Jc. All rights reserved.
+//
+
+import UIKit
+
+class HeaderProfile: UIView {
+
+}
